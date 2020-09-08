71ae5b6628bd8f3b31ceba72bb8b989c07fa981f
london-newyork <yuriwork01@gmail.com>
2020-09-08 13:19:35 +0000

    follow, unfollow

--- app/Http/Controllers/TasksController.php
+++ app/Http/Controllers/TasksController.php
@@ -15,12 +15,29 @@ class TasksController extends Controller
         $tasks = Task::all();
 
         // タスク一覧ビューでそれを表示
-        return view('tasks.index', [
-            'tasks' => $tasks,
-        ]);
+        //return view('tasks.index', [
+         //   'tasks' => $tasks,
+        //]);
+        
+        $data = [];
+        if (\Auth::check()) { // 認証済みの場合
+            // 認証済みユーザを取得
+            $user = \Auth::user();
+            // ユーザの投稿の一覧を作成日時の降順で取得
+            $tasks = $user->tasks()->orderBy('created_at', 'desc')->paginate(10);
+
+            $data = [
+                'user' => $user,
+                'tasks' => $tasks,
+            ];
+        }
+
+        // Welcomeビューでそれらを表示
+        return view('welcome', $data);
+        
     }
 
-    // 中略
+
 
     // getでtasks/createにアクセスされた場合の「新規登録画面表示処理」
     public function create()
